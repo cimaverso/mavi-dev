d573df39f3d9373075854e4a1820dc610bd01864
WolfWerkDev <samalepuhu@gmail.com>
2026-03-03 21:15:29 -0500

    Refactor para recibir info segun el caso

--- backend/app/schemas/proveedor.py
+++ backend/app/schemas/proveedor.py
@@ -1,10 +1,20 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
-class ProveedorRead(BaseModel):
-    prov_id: int
+class ProveedorBase(BaseModel):
     prov_nombre: str
-    prov_telefono: str | None
+    prov_telefono: Optional[str] = None
+
+class ProveedorRead(ProveedorBase):
+    prov_id: int
+
+class ProveedorCreate(ProveedorBase):
+    pass
+
+class ProveedorUpdate(BaseModel):
+    prov_nombre: Optional[str] = None
+    prov_telefono: Optional[str] = None
 
     class Config:
         from_attributes = True  
