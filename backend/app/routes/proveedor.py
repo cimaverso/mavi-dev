e2240ad2425f9764c0ab6d8bb95d959aabe468ed
WolfWerkDev <samalepuhu@gmail.com>
2026-03-03 20:13:24 -0500

    Se agregaron metodos POST y PUT para proveedores

--- backend/app/routes/proveedor.py
+++ backend/app/routes/proveedor.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from app.core.db import get_db
 from app.schemas.proveedor import ProveedorRead
 from app.services.proveedor import ProveedorServicio
+from models.proveedores import Proveedor
 
 router = APIRouter(
     prefix="/proveedores",
@@ -25,4 +26,25 @@ def obtener_proveedor(proveedor_id: int, db: Session = Depends(get_db)):
     if not proveedor:
         raise HTTPException(status_code=404, detail="Proveedor no encontrado")
 
+    return proveedor
+
+@router.post("/", response_model=ProveedorRead)
+def crear_proveedor(proveedor: Proveedor, db: Session = Depends(get_db)):
+    proveedor = ProveedorServicio.crear(db, proveedor)
+    if not proveedor:
+       raise HTTPException(
+          status_code=400,
+          detail="Error al registrar proveedor"
+       )
+    
+    return proveedor
+
+@router.put("/", response_model=ProveedorRead)
+def actualizar_proveedor(proveedor: Proveedor, db: Session = Depends(get_db)):
+    proveedor = ProveedorServicio.actualizar_proveedor(db, proveedor)
+    if not proveedor:
+        raise HTTPException(
+            status_code=400,
+            detail="Error actualizando la información del proveedor"
+        )
     return proveedor
